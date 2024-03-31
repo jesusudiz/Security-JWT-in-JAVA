5617be3220bded7c992085a49db59d422ca91177
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2024-03-30 23:19:51 -0300

    create userrepository

--- src/main/java/com/security/securityJWT/persistence/repository/UserRepository.java
+++ src/main/java/com/security/securityJWT/persistence/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.security.securityJWT.persistence.repository;public class UserRepository {
+package com.security.securityJWT.persistence.repository;
+
+
+import com.security.securityJWT.persistence.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
 }
