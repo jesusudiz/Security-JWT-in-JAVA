a38525a918def4a9ab60f9fe80f1f0eb35957a4d
Jesús Udiz <jesus.udiz@bettercommerce.cl>
2024-03-30 23:19:06 -0300

    modificated controller

--- src/main/java/com/security/securityJWT/controller/TestAuthController.java
+++ src/main/java/com/security/securityJWT/controller/TestAuthController.java
@@ -1,2 +1,34 @@
-package com.security.securityJWT.controller;public class TestAuthController {
+
+package com.security.securityJWT.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class TestAuthController {
+
+    @GetMapping("/get")
+    public String helloGet(){
+        return "Hello World - GET";
+    }
+
+    @PostMapping("/post")
+    public String helloPost(){
+        return "Hello World - POST";
+    }
+
+    @PutMapping("/put")
+    public String helloPut(){
+        return "Hello World - PUT";
+    }
+
+    @DeleteMapping("/delete")
+    public String helloDelete(){
+        return "Hello World - DELETE";
+    }
+
+    @PatchMapping("/patch")
+    public String helloPatch(){
+        return "Hello World - PATCH";
+    }
 }
